2134a4eb68df05576b129c67b250c2515536910d
koheisanno <kohei@gmail.com>
2023-12-06 13:41:04 -0500

    test

--- iptable/xdp/xdp.c
+++ iptable/xdp/xdp.c
@@ -6,5 +6,21 @@
 SEC("xdp")
 int xdp_iptable(struct xdp_md *ctx)
 {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+    __u16 h_proto;
+
+    // Check if packet is large enough to contain an Ethernet header
+    if (data + sizeof(struct ethhdr) > data_end)
+        return XDP_DROP;
+
+    // get the protocol from the Ethernet header
+    h_proto = eth->h_proto;
+
+    // drop if the protocol is IPv6
+    if (h_proto == htons(ETH_P_IPV6))
+        return XDP_DROP;
+
     return XDP_DROP;
 }
