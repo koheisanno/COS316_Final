fac5c0652d613f83523746da13673d230fb96acf
koheisanno <kohei@gmail.com>
2023-12-06 22:01:55 -0500

    test

--- iptable/xdp/xdp.c
+++ iptable/xdp/xdp.c
@@ -1,30 +1,38 @@
-#include "bpf_helpers.h"
+#include <linux/bpf.h>
+#include <linux/ip.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h>
+#include <arpa/inet.h>
+#include <sys/queue.h>
 
+struct bpf_map_def {
+	unsigned int type;
+	unsigned int key_size;
+	unsigned int value_size;
+	unsigned int max_entries;
+};
+struct __create_map_def {
+  const char *name;
+  void *map_data;  // Mock version only: holds head to single linked list of map
+                   // items
+  struct bpf_map_def *map_def;
+  SLIST_ENTRY(__create_map_def) next;
+};
 
+// Declaration only. Definition held in mock_map package.
+SLIST_HEAD(__maps_head_def, __create_map_def);
+extern struct __maps_head_def *__maps_head;
 
-// Ethernet header
-struct ethhdr {
-  __u8 h_dest[6];
-  __u8 h_source[6];
-  __u16 h_proto;
-} __attribute__((packed));
+#define BPF_MAP_DEF(x) static struct bpf_map_def x
 
-// IPv4 header
-struct iphdr {
-  __u8 ihl : 4;
-  __u8 version : 4;
-  __u8 tos;
-  __u16 tot_len;
-  __u16 id;
-  __u16 frag_off;
-  __u8 ttl;
-  __u8 protocol;
-  __u16 check;
-  __u32 saddr;
-  __u32 daddr;
-} __attribute__((packed));
-
-
+#define BPF_MAP_ADD(x)                                          \
+  static __attribute__((constructor)) void __bpf_map_##x() {    \
+    static struct __create_map_def __bpf_map_entry_##x;         \
+    __bpf_map_entry_##x.name = #x;                              \
+    __bpf_map_entry_##x.map_data = NULL;                        \
+    __bpf_map_entry_##x.map_def = &x;                           \
+    SLIST_INSERT_HEAD(__maps_head, &__bpf_map_entry_##x, next); \
+  }
 
 BPF_MAP_DEF(blacklist) = {
     .map_type = BPF_MAP_TYPE_LPM_TRIE,
