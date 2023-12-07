d2d46ccc2d13bd5462c0830e186ea2352edf03e4
koheisanno <kohei@gmail.com>
2023-12-06 22:19:08 -0500

    test

--- iptable/bpf/xdp.c
+++ iptable/bpf/xdp.c
@@ -1,30 +1,7 @@
 #include "bpf_helpers.h"
 #include <arpa/inet.h>
 #include <linux/if_ether.h>
-
-
-// Ethernet header
-struct ethhdr {
-  __u8 h_dest[6];
-  __u8 h_source[6];
-  __u16 h_proto;
-} __attribute__((packed));
-
-// IPv4 header
-struct iphdr {
-  __u8 ihl : 4;
-  __u8 version : 4;
-  __u8 tos;
-  __u16 tot_len;
-  __u16 id;
-  __u16 frag_off;
-  __u8 ttl;
-  __u8 protocol;
-  __u16 check;
-  __u32 saddr;
-  __u32 daddr;
-} __attribute__((packed));
-
+#include <linux/ip.h>
 
 
 BPF_MAP_DEF(blacklist) = {
